58872c8d5e5263b799f4a5f9084030b601fd38bd
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-02-22 15:41:35 +0200

    added mobile css

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const buttons = document.querySelectorAll('.buttons button');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    document.querySelector('main').classList.add("displayNone")
+    document.querySelector('main').classList.add("displayNone");
+    document.querySelector('main').classList.remove("displayFlex");
+
     const buttonText = button.textContent;
     fetch('./data/data.json').then(res => res.json()).then(data => {
       let selectedTab = data.tabs.find(tab => tab.name === buttonText);
